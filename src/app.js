c8eaff84dfa0d3b54b0b698a3dc1c5b2caadcef9
Amandeep362 <tarlok6667@gmail.com>
2022-08-18 18:44:17 +0530

    commit changes

---
+++ src/app.js
@@ -0,0 +1,21 @@
+
+import MyClass from "./MyClass";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { Link } from "@material-ui/core";
+
+const App = () => {
+return(
+    <>
+   
+        
+      <Routes>
+        <Route
+          path="/my-class"
+          element={<MyClass />}
+        />
+       
+      </Routes>
+    </>
+ )
+}
+export default App;
